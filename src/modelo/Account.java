4f6d884a060ef00d27f50f7faed4f0ed32c713ff
David <davidmaringomez81@gmail.com>
2023-02-19 10:07:15 +0100

    Punto 6 - Terminado

--- src/modelo/Account.java
+++ src/modelo/Account.java
@@ -76,10 +76,12 @@ public class Account implements java.io.Serializable {
 	public void setAccMovementsDest(Set<AccMovement> accMovementsDest) {
 		this.accMovementsDest = accMovementsDest;
 	}
+	
+	
 
 	@Override
 	public String toString() {
-		return "Account [accountno=" + accountno + "]";
+		return "Account [accountno=" + accountno + ", emp=" + emp.getEmpno() + ", amount=" + amount + "]";
 	}
 	
 	
